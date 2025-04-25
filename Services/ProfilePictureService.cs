828e62622be323db47db8434cb611b79fbe5a2d9
HP <HP@LAPTOP-Q0VM2EGN>
2025-04-25 11:30:59 +0800

    Add project files.

---
+++ Services/ProfilePictureService.cs
@@ -0,0 +1,14 @@
+﻿namespace GEMS.Services
+{
+    public class ProfilePictureService
+    {
+        public string ProfilePictureUrl { get; private set; } = "/images/profile-picture.png";
+
+        public event Action? OnProfilePictureChanged;
+        public void SetProfilePictureUrl(string url)
+        {
+            ProfilePictureUrl = url;
+            OnProfilePictureChanged?.Invoke();
+        }
+    }
+}
